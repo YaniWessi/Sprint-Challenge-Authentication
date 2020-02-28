2f5e8e8f69303b48a8dcc1f9f8168dcb5e961fd5
Kaela M Gallo <kaelamgallo@Kaelas-MBP.home>
2020-02-28 13:31:30 -0500

    created a user and logined him in

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,27 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const bcrypt = require("bcryptjs");
+
+const Users = require("../jokes/jokes-model.js");
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  let { username, password } = req.headers;
+
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: "Invalid Credentials" });
+        }
+      })
+      .catch(({ name, message, stack }) => {
+        res.status(500).json({ name, message, stack });
+      });
+  } else {
+    res.status(401).json({ you: "shall not pass!" });
+  }
 };
---
+++ jokes/jokes-model.js
@@ -0,0 +1,29 @@
+const db = require("../database/dbConfig");
+
+module.exports = {
+  add,
+  findBy,
+  findById
+};
+
+function findBy(filter) {
+  return db("users")
+    .select("id", "username", "password")
+    .where(filter);
+}
+
+function add(user) {
+  return db("users")
+    .insert(user, "id")
+    .then(ids => {
+      const [id] = ids;
+      return findById(id);
+    });
+}
+
+function findById(id) {
+  return db("users")
+    .select("id", "username")
+    .where({ id })
+    .first();
+}
